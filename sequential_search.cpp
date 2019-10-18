a4a47fae69fe1ae9ed369e8753453fd847342544
Thereza Angélica <48398064+itstherall@users.noreply.github.com>
2019-10-17 23:52:26 -0300

    Add files via upload

---
+++ sequential_search.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+//sequential search algorithm 
+
+void seq_search(int *v , int size, int key)
+{
+	//creat a var temporary for know if u get the menssage if the algorithm don't find de key
+	int tmp;
+
+	for(int i = 0 ; i < size ; ++i)
+	{
+		if(v[i]== key)
+		{
+			cout << "Find the key in the vector!\n";
+			tmp = 0;
+			return;
+		}
+		else
+		{
+			tmp = 1;
+		}
+	}
+
+	if(tmp == 1)
+	{
+		cout << "Don't find the key!\n";
+	}
+}
+
+
+int main ()
+{
+	int *v, size, key;
+
+	cout << "what's size of vector?\n";
+	cin >> size;
+
+	//aloc a vector with size
+	v = new int[size];
+
+	//run for the vector and get the elements
+	cout << "set elements of vector: \n";
+	for (int i=0 ; i < size ; ++i)
+	{
+		cin >> v[i];
+	}	
+
+	//take a key for search
+	cout << "what's the key u looking for?\n";
+	cin >> key;
+
+	seq_search( v, size, key);
+
+	return 0;
+}
